01f516936d37abe2d4b29414555220b85c2595b9
Thais Almeida <90707459+thais36@users.noreply.github.com>
2022-05-27 22:54:11 -0300

    Add files via upload

--- pessoas.sql
+++ pessoas.sql
@@ -1,8 +1,18 @@
 CREATE DATABASE pessoas;
 
+USE pessoas;
+
 CREATE TABLE pessoas (
+id INT NOT NULL PRIMARY KEY,
 nome VARCHAR (70),
 nascimento DATE,
 altura INT
 
-)
+);
+
+INSERT INTO pessoas (nome, nascimento) VALUES (1,'Thais', '1991-09-24');
+
+SELECT* FROM pessoa;
+
+UPDATE pessoas SET nome='Thais Oliveira' WHERE ID=1;
+
